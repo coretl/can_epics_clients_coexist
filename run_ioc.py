cfeb69ad32b9c710c622adf26767f98322cf359d
Tom Cobb <tom.cobb@diamond.ac.uk>
2023-04-05 09:55:56 +0100

    Any better?

--- run_ioc.py
+++ run_ioc.py
@@ -1,27 +1,6 @@
-import subprocess
-import sys
 from pathlib import Path
+from epicscorelibs.ioc import start_ioc
 import time
-import signal
-
-def start_ioc_subprocess() -> subprocess.Popen:
-    here = Path(__file__).absolute().parent
-    args = [sys.executable, "-m", "epicscorelibs.ioc", "-d", str(here/"record.db")]
-    process = subprocess.Popen(
-        args,
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        universal_newlines=True,
-    )
-    return process
-
-process = start_ioc_subprocess()
-
-def stop_process(signum, frame):
-    print("****IOC output")
-    print(process.communicate("exit")[0])
-    sys.exit()
-
-signal.signal(signal.SIGTERM, stop_process)
+here = Path(__file__).absolute().parent
+start_ioc(str(here/"record.db"))
 time.sleep(60)
